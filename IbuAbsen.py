e0795ac0f93eea3c251693e7aa201b1896e9c414
uyud <yudhahamdi@gmail.com>
2022-01-25 00:29:34 +0700

    berhasil absen

--- IbuAbsen.py
+++ IbuAbsen.py
@@ -1,14 +1,13 @@
 # bot.py
 import os
-import random
-
 import discord
 from dotenv import load_dotenv
 
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
@@ -25,9 +24,13 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == client.user:
         return
-
     elif message.content == 'absen bu':
-        await message.channel.send('ia nanti yaaa!!')
+        names = list()
+        channel = client.get_channel(934690619559936024)
+        for member in channel.members:
+            # print(f'{member.name} !!!')
+            names.append(f'{member.name}')
+        await message.channel.send('\n'.join(names))
     elif message.content == 'hi bu':
         response = 'Selamat pagi  ' + str(message.author)
         await message.channel.send(response)
